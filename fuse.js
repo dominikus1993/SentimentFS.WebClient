d9ca40584dafae1d92d24446b1af5b716f7c20c1
Dominik Kotecki <dominikus1910@gmail.com>
2017-03-19 20:29:01 +0100

    add index.html

--- fuse.js
+++ fuse.js
@@ -5,7 +5,7 @@ const { FuseBox, SVGPlugin, CSSPlugin, BabelPlugin } = require("fuse-box");
 let fuse = new FuseBox({
     homeDir: "src/",
     sourcemaps: true,
-    outFile: "./dist/bundle.js",
+    outFile: "./public/bundle.js",
     plugins: [
         SVGPlugin(),
         CSSPlugin(),
