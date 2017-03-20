2485379dfdfd79b576996f2796943389dd9f95ff
dominik.kotecki <dominik.kotecki@rossmann.com.pl>
2017-03-20 11:51:34 +0100

    fuse -> webpack2

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const babelOptions = {
+    "presets": [
+        [
+            "es2015",
+            {
+                "modules": false
+            }
+        ],
+        "es2016"
+    ]
+};
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [{
+            test: /\.ts(x?)$/,
+            exclude: /node_modules/,
+            use: [
+                {
+                    loader: 'babel-loader',
+                    options: babelOptions
+                },
+                {
+                    loader: 'ts-loader'
+                }
+            ]
+        }, {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [
+                {
+                    loader: 'babel-loader',
+                    options: babelOptions
+                }
+            ]
+        }, {
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader"
+            }, {
+                loader: "sass-loader",
+                options: {
+                    includePaths: ["absolute/path/a", "absolute/path/b"]
+                }
+            }]
+        }]
+    },
+    plugins: [
+        new UglifyJSPlugin()
+    ],
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "public"),
+        compress: true,
+        port: 9000
+    }
+};
