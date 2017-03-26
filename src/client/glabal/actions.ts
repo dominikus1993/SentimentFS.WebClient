401242c38ffe2304c2bd2629e5d456cc68ad22c2
Dominik Kotecki <dominikus1910@gmail.com>
2017-03-26 17:23:10 +0200

    fix

--- src/client/glabal/actions.ts
+++ src/client/glabal/actions.ts
@@ -1,8 +1,8 @@
-import { IResponse, IResult } from "./models";
+import { IResult } from "./models";
 
 export type HistoryActions = { type: "HISTORY", payload: any }
     | { type: "HISTORY_PENDING" }
     | { type: "HISTORY_REJECTED", payload?: any }
-    | { type: "HISTORY_FULFILLED", payload: IResponse<IResult<string[]>> };
+    | { type: "HISTORY_FULFILLED", payload: IResult<string[]> };
 
 export type Actions = HistoryActions;
