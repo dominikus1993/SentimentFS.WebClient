350d177343e5e08c87ee1378cbbdff69dea09b7b
Dominik Kotecki <dominikus1910@gmail.com>
2017-03-26 14:16:31 +0200

    small changes

--- src/client/glabal/reducers.ts
+++ src/client/glabal/reducers.ts
@@ -1,6 +1,30 @@
+import { InitialAnalysisScore } from "./constants";
+import { IAnalysisScore } from "./models";
 
-import { Action } from "./actions";
+import { Actions } from "./actions";
 
-export type Reducer<TState> = (state: TState, action: Action) => TState;
+export type Reducer<TState> = (state: TState, action: Actions) => TState;
 
-export interface IReducers {}
+export type Reducers = {
+    readonly analysisScore: Reducer<IAnalysisScore>;
+    readonly history: Reducer<string[]>;
+};
+
+export function history(state: string[] = [], action: Actions) {
+    if (action.type === "HISTORY_FULFILLED") {
+        if (action.payload.data.isSuccess) {
+            return action.payload.data.value;
+        }
+        return state;
+    }
+    return state;
+}
+
+export function analysisScore(state: IAnalysisScore = InitialAnalysisScore, action: Actions){
+    return state;
+}
+
+export const reducers: Reducers = {
+    analysisScore,
+    history,
+};
--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,9 +1,19 @@
-import "tslib";
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { Provider } from "react-redux";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import App from "./components/App";
+import { Provider } from "react-redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import * as logger from "redux-logger";
+import "tslib";
+import App from "./containers/App";
+import { InittialApplicationState } from "./glabal/constants";
+import { IApplicationState } from "./glabal/models";
+import { reducers } from "./glabal/reducers";
+
+const combinedReducers  = combineReducers<IApplicationState>(reducers);
+const middleware = applyMiddleware(logger());
+const store = createStore<IApplicationState>(combinedReducers, InittialApplicationState, middleware);
+
+store.dispatch({type: "HISTORY_FULFILLED", payload: {data: {isSuccess: true, value: ["dupa"]}} as any });
 
 export const render = (element) => {
     ReactDOM.render(<App message="Hello World from App" ></App>, document.querySelector(element));
