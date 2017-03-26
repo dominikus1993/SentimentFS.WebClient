5810e263939a7ec707a00730bc985697497a7c76
Dominik Kotecki <dominikus1910@gmail.com>
2017-03-26 19:23:26 +0200

    fix

--- src/client/history/reducers.ts
+++ src/client/history/reducers.ts
@@ -1,8 +1,8 @@
 import { Actions } from "../glabal/actions";
 export function history(state: string[] = [], action: Actions) {
     if (action.type === "HISTORY_FULFILLED") {
-        if (action.payload.data.isSuccess) {
-            return action.payload.data.value;
+        if (action.payload.isSuccess) {
+            return action.payload.value;
         }
         return state;
     }
