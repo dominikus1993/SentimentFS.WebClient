80a13f2d9f92f468f239e5e728a70ffcec373c75
dominik.kotecki <dominik.kotecki@rossmann.com.pl>
2017-03-27 15:33:15 +0200

    test

--- src/client/history/__tests__/HistoryComponent.tsx
+++ src/client/history/__tests__/HistoryComponent.tsx
@@ -6,4 +6,5 @@ import { History } from "../HistoryComponent";
 test("<History />", () => {
     const keyWords: IKeyWord[] = [{key: "Dominik", quantity: 10}];
     const component = shallow(<History keyWords= {keyWords}/>);
+    expect(1).toBe(1);
 });
