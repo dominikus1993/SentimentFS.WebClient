44ad6fedb63ff7e325586def316afc94ad1273e3
Dominik Kotecki <dominikus1910@gmail.com>
2017-03-31 20:18:05 +0200

    add SearchBox component

--- src/client/searchBox/SearchBoxComponent.tsx
+++ src/client/searchBox/SearchBoxComponent.tsx
@@ -1,11 +1,22 @@
-import { Actions } from "../glabal/actions";
 import * as React from "react";
+import { Actions } from "../glabal/actions";
 
-interface ISearchBoxProps {
+export interface ISearchBoxProps {
     readonly onSearch: (value: string) => Actions;
     readonly isLoading: boolean;
 }
 
-interface ISearchBoxState {
+export interface ISearchBoxState {
     text: string;
+}
+
+export default class SearchBoxComponent extends React.Component<ISearchBoxProps, ISearchBoxState> {
+    constructor(props) {
+        super(props);
+        this.setState({ text: "" });
+    }
+
+    public render() {
+        return (<div>Test</div>);
+    }
 }
