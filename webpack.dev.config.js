a26c5754e8f4ee24deff26035ab5d307211d899a
Dominik Kotecki <dominikus1910@gmail.com>
2017-04-02 21:17:19 +0200

    debuging

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,63 +1,70 @@
-const path = require('path');
-
+const path = require('path')
+const webpack = require('webpack')
 const babelOptions = {
-    "presets": [
-        "react", [
-            "es2015",
-            {
-                "modules": false
-            }
-        ],
-        "es2016"
-    ]
-};
+  'presets': [
+    'react', [
+      'es2015',
+      {
+        'modules': false
+      }
+    ],
+    'es2016'
+  ]
+}
 
 module.exports = {
-    entry: './src/index.tsx',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public')
-    },
-    module: {
-        rules: [{
-            test: /\.ts(x?)$/,
-            exclude: /node_modules/,
-            use: [{
-                    loader: 'babel-loader',
-                    options: babelOptions
-                },
-                {
-                    loader: 'ts-loader'
-                }
-            ]
-        }, {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: [{
-                loader: 'babel-loader',
-                options: babelOptions
-            }]
-        }, {
-            test: /\.scss$/,
-            use: [{
-                loader: "style-loader"
-            }, {
-                loader: "css-loader"
-            }, {
-                loader: "sass-loader",
-                options: {
-                    includePaths: ["absolute/path/a", "absolute/path/b"]
-                }
-            }]
+  cache: true,
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['source-map-loader'],
+        enforce: 'pre'
+      }, {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: babelOptions
+        },
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }, {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: babelOptions
         }]
-    },
-    plugins: [],
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "public"),
-        compress: true,
-        port: 9000
-    }
-};
+      }, {
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'sass-loader',
+          options: {
+            includePaths: ['absolute/path/a', 'absolute/path/b']
+          }
+        }]
+      }]
+  },
+  plugins: [],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 9000
+  }
+}
